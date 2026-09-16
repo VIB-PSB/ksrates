d27a3c2d6c516649abae0171dfb7d361a95a3d57
Cecilia-Sensalari <ceci.sensa93@gmail.com>
2026-09-16 13:47:33 +0200

    Extracting Ks values can use SQLite

--- ksrates/fc_extract_ks_list.py
+++ ksrates/fc_extract_ks_list.py
@@ -15,12 +15,28 @@ def ks_list_from_tsv(tsv_file, max_ks, data_type, min_ks=0.005):
     :return: ks_list_filtered, list of either whole-paranome, anchors pairs, reciprocally retained or ortholog Ks values
     :return: weight_list_filtered, list of weights for Ks values (not returned for orthologs Ks)
     """
-    with open(tsv_file, "r") as tsv_file:
-        tsv = read_csv(tsv_file, sep="\t")
+    with open(tsv_file, "r") as f:
+        tsv = read_csv(f, sep="\t")
+    return ks_list_from_df(tsv, max_ks, data_type, min_ks=min_ks)
 
+
+def ks_list_from_df(df, max_ks, data_type, min_ks=0.005):
+    """
+    Same as ks_list_from_tsv, but takes an already-loaded DataFrame instead of reading a TSV file
+    from disk. Useful when the caller already has the data in memory (e.g. reconstructed from the
+    paralog Ks database) and doesn't want to pay for a redundant read.
+
+    :param df: DataFrame with the same columns as a wgd Ks TSV file (at minimum: Family, Node, Ks,
+               AlignmentCoverage, AlignmentIdentity, AlignmentLength)
+    :param max_ks: maximum Ks value to be accepted for the analysis
+    :param data_type: specifies the nature of the Ks values in df (either "paralogs", "anchor pairs", "reciprocally retained" or "orthologs")
+    :param min_ks: minimum Ks value to be accepted for the analysis
+    :return: ks_list_filtered, list of either whole-paranome, anchors pairs, reciprocally retained or ortholog Ks values
+    :return: weight_list_filtered, list of weights for Ks values (not returned for orthologs Ks)
+    """
     # Re-calculate the weights when excluding the outliers (i.e. Ks > max_ks)
-    # Return the Ks TSV file with the updated WeightOutliersExcluded column
-    filtered_tsv_updated_weights = filter_compute_weights(tsv, min_ks, max_ks)
+    # Return the Ks data with the updated WeightOutliersExcluded column
+    filtered_tsv_updated_weights = filter_compute_weights(df, min_ks, max_ks)
 
     ks_list_filtered = filtered_tsv_updated_weights["Ks"].to_list()
     weight_list_filtered = filtered_tsv_updated_weights["WeightOutliersExcluded"].to_list()
